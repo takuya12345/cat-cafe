62b9122145485658ca269312e04b96c6b57084e2
takuya12345 <10l.mah.s11@gmail.com>
2023-01-07 17:09:54 +0900

    更新処理作成

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\Admin\AdminBlogController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -23,3 +24,9 @@ Route::get('/', function () {
 Route::get('contact', [ContactController::class, 'index'])->name('contact');
 Route::post('contact', [ContactController::class, 'sendMail']);
 Route::get('contact/complete', [ContactController::class, 'complete'])->name('contact.complete');
+
+// ブログ
+Route::get('/admin/blogs', [AdminBlogController::class, 'index'])->name('admin.blogs.index');
+Route::get('/admin/blogs/create', [AdminBlogController::class, 'create'])->name('admin.blogs.create');
+Route::post('/admin/blogs', [AdminBlogController::class, 'store'])->name('admin.blogs.store');
+Route::get('admin/blogs/{blog}', [AdminBlogController::class, 'edit'])->name('admin.blogs.edit');
